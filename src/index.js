413194ebc44182a6ee37f0b5e4d16a0e114e4cec
Cesar <cesr.sntn@gmail.com>
2022-08-22 11:56:58 -0400

    create funtionality between tasks and projects

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-const submit = document.querySelector("#submit1");
 
 import "./style.css"
 require("./style.css");
 
-import { createProject } from "./projects";
-
-// consoleit();
+import {openForm,createProject,cancelForm, projectProjects,openTodo, cancelTodo, createTodo} from "./projects";
 
 
+const submit1 = document.getElementById("submit1"); //gets submit button from html
+submit1.onclick = function(){createProject(); projectProjects();}
 
-submit.addEventListener("click", function(){
-    createProject();
-})
+const createProject1 = document.getElementById("createProject");
+createProject1.onclick = function(){openForm();}
+
+const cancel1 = document.getElementById("cancel1");
+cancel1.onclick = function(){cancelForm();}
+
+
+// -------------------------------------------------------
+
+
+
+
+
+
 
--- src/projects.js
+++ src/projects.js
@@ -1,9 +1,9 @@
 
-const submit = document.querySelector("#submit1");
-const cancel = document.querySelector("#cancel");
+
+const content1 = document.querySelector("#content1");
+const form1 = document.getElementById("#form1");
 
 let projectList = [];
-
 
 class project{
     constructor (projectName, projectDesc){
@@ -13,19 +13,169 @@ class project{
     pushp(){
         projectList.push(this);
     }
-    log(){
-        for(let obj of projectList){
-            console.log(obj);
-            console.log(projectList)
-        }
-        
-    }
 }
+
 export function createProject(){
 const pName = document.querySelector("#task1").value;
 const pDesc = document.querySelector("#desc1").value;
 
 const newProject = new project(pName, pDesc)
 newProject.pushp();
-newProject.log();
+content1.style.display = "none";
+projectList.forEach(item =>{
+    console.log(item["projectDesc"]);
+    
+
+})
+
+
 }
+
+export function openForm(){
+    content1.style.display = "flex";
+}
+export function cancelForm(){
+    content1.style.display = "none";
+}
+
+const contentDisplay = document.getElementById("contentDisplay1");
+
+export function projectProjects(){
+
+    createNewDivs();
+    
+}
+
+
+
+
+
+// ------------------------------------------------------------------
+
+
+const content2 = document.getElementById("content2");
+export function openTodo(){
+    content2.style.display = "flex";
+    
+}
+
+export function cancelTodo(){
+    content2.style.display = "none";
+
+}
+
+function getTodo(task, date){
+    this.task = task;
+    this.date = date;
+
+    
+}
+
+
+
+
+
+function createNewDivs(){
+
+    const division2 = document.getElementById("division2");
+
+    contentDisplay.textContent = "";
+
+    projectList.forEach(item =>{
+
+        const newDiv = document.createElement("button");
+        newDiv.classList.add("projectOutline");
+        newDiv.addEventListener("click", function(){
+
+            division2.textContent = "";
+            division2.appendChild(newMainDiv);
+
+        })
+
+        newDiv.textContent = `${item["projectName"]}`;
+        contentDisplay.appendChild(newDiv);
+
+        const newMainDiv = document.createElement("div");
+        newMainDiv.classList.add("newMainDiv");
+
+        const div2cont = document.createElement("div")
+            div2cont.classList.add("div2cont");
+            newMainDiv.appendChild(div2cont);
+
+        const title2Cont = document.createElement("div");
+            title2Cont.classList.add("title2Cont");
+            div2cont.appendChild(title2Cont);
+
+        const title2 = document.createElement("div");
+            title2.classList.add("title2");
+            title2Cont.appendChild(title2);
+        
+        const TASK = document.createElement("div");
+            TASK.textContent = `${item["projectName"]}`;
+            title2.appendChild(TASK);
+
+
+        const desc2 = document.createElement("div");
+            desc2.classList.add("desc2");
+            title2Cont.appendChild(desc2);
+
+        const DESC = document.createElement("div");
+            DESC.textContent = `${item["projectDesc"]}`
+            desc2.appendChild(DESC);
+
+        const createTodoButton = document.createElement("button");
+        createTodoButton.textContent = "fdsifamoim";
+            createTodoButton.classList.add("createTodo");
+            div2cont.appendChild(createTodoButton);
+
+        //onclick do:
+        createTodoButton.addEventListener("click", function(){
+            createTodos();
+        })
+
+            function createTodos(){
+                    
+                const todoDisplay = document.createElement("div");
+                    todoDisplay.classList.add("todoDisplay");
+                    newMainDiv.appendChild(todoDisplay);
+            
+                const taskDiv = document.createElement("div");
+                    taskDiv.classList.add("taskDiv");
+                    todoDisplay.appendChild(taskDiv);
+            
+                const taskTitle = document.createElement("div");
+                    taskTitle.textContent = "task example"
+                    taskDiv.appendChild(taskTitle);
+            
+                const taskDesc = document.createElement("div");
+                    taskDesc.textContent = "task Desc"
+                    taskDiv.appendChild(taskDesc);
+            
+            }
+
+    })
+}
+
+function createTodos(){
+    const newMainDiv = document.querySelector(".newMainDiv");
+        
+
+
+    const todoDisplay = document.createElement("div");
+        todoDisplay.classList.add("todoDisplay");
+        newMainDiv.appendChild(todoDisplay);
+
+    const taskDiv = document.createElement("div");
+        taskDiv.classList.add("taskDiv");
+        todoDisplay.appendChild(taskDiv);
+
+    const taskTitle = document.createElement("div");
+        taskTitle.textContent = "task example"
+        taskDiv.appendChild(taskTitle);
+
+    const taskDesc = document.createElement("div");
+        taskDesc.textContent = "task Desc"
+        taskDiv.appendChild(taskDesc);
+
+}
+
